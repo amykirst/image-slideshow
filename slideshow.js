89b6a6302c5092149778696b18b47127fc43c4bc
Amy Kirst <amylkirst@gmail.com>
2015-06-27 20:48:09 -0400

    Formatted code; made compatible with IE 8

--- slideshow.js
+++ slideshow.js
@@ -1,12 +1,3 @@
-/* To Do
- 
- IE 8 is not calculating the position of the controls the same as Chrome
-  - Maybe add an event listener if window is resized?
-  - When the window is resized, the image width and height also change in IE, but the controls aren't being repositioned; need an event
- In IE 8, controls do not stay in the middle if window is resized
- 
- */
-
 // Get array with photos
 var photos = document.getElementsByTagName("figure"); // Returns object of photos
 var numPhotos = photos.length;
@@ -14,80 +5,73 @@ var shownPhotoIndex;
 
 // Hide all photos except first by giving class of "hide" (if JS is not enabled, all photos will show)
 function hidePhotos() {
-  for (var i = 0; i < numPhotos; i++) {
-   if (photos[i] !== photos[0]) {
-      photos[i].className += " hide";
-           
-    } // end if statement
-    
-  } // end for statement
-} // end hidePhotos
+    for (var i = 0; i < numPhotos; i++) {
+      if (photos[i] !== photos[0]) {
+        photos[i].className += " hide";
+      } // end if statement
+    } // end for statement
+  } // end hidePhotos
 
-
-// Add controls (if JS is not enabled; controls will not be present)
+function positionButtons() {
+    var getImage = photos[0].children[0]; // IE 8 does not support photos[0].firstElementChild;
+    var imageHeight = (getImage.height);
+    var imageWidth = (getImage.width);
+    var paddingTop = (imageHeight / imageWidth) * 100;
+    paddingTop = +paddingTop.toFixed(2);
+    var percent = paddingTop + "%";
+    document.getElementById("previous").style.paddingBottom = percent;
+    document.getElementById("next").style.paddingBottom = percent;
+  }
+  
+  // Add controls (if JS is not enabled; controls will not be present)
 function addControls() {
-  var slideshow = document.getElementById("slideshow");
-  // Create buttons
-  var spanNext = document.createElement("span");
-  var spanPrevious = document.createElement("span");
-  // Give buttons IDs
-  spanNext.setAttribute("id", "next");
-  spanPrevious.setAttribute("id", "previous");
-  // Add buttons to slideshow div
-  slideshow.appendChild(spanNext);
-  slideshow.appendChild(spanPrevious);
-  // Add content to buttons
-  document.getElementById("next").innerHTML = "<p>>></p>";
-  document.getElementById("previous").innerHTML = "<p><<</p>";
-  // Calculate position of buttons
-  var getImage = photos[0].children[0]; // IE 8 does not support photos[0].firstElementChild;
-  var imageHeight = (getImage.height); 
-  var imageWidth = (getImage.width); 
-  var paddingTop = (imageHeight / imageWidth) * 100;
-  paddingTop = +paddingTop.toFixed(2);
-  var percent = paddingTop + "%";
-  document.getElementById("previous").style.paddingBottom = percent; 
-  document.getElementById("next").style.paddingBottom = percent;
-} // end addControls
+    var slideshow = document.getElementById("slideshow");
+    // Create buttons
+    var spanNext = document.createElement("span");
+    var spanPrevious = document.createElement("span");
+    // Give buttons IDs
+    spanNext.setAttribute("id", "next");
+    spanPrevious.setAttribute("id", "previous");
+    // Add buttons to slideshow div
+    slideshow.appendChild(spanNext);
+    slideshow.appendChild(spanPrevious);
+    // Add content to buttons
+    document.getElementById("next").innerHTML = "<p>>></p>";
+    document.getElementById("previous").innerHTML = "<p><<</p>";
+    // Calculate position of buttons
+    positionButtons();
+  } // end addControls
+  // Find curently shown photo
 
-
-// Find curently shown photo
 function findShownPhoto() {
-  for (var i = 0; i < numPhotos; i++) {
-    // if the image does not contain a class of "hide"
-    if (photos[i].className.indexOf("hide") == -1) {
+    for (var i = 0; i < numPhotos; i++) {
+      // if the image does not contain a class of "hide"
+      if (photos[i].className.indexOf("hide") == -1) {
         shownPhotoIndex = i;
-    }  // end if statement  
-  } // end for statement
-} // end findShownPhoto
-
-
+      } // end if statement  
+    } // end for statement
+  } // end findShownPhoto
 
 function progressSlides() {
     var next = document.getElementById("next");
-    
     // When the next button is clicked, show next photo
     next.onclick = function() {
         var nextPhoto;
         findShownPhoto();
-        
         // If current photo is last photo, go to first photo
         if (shownPhotoIndex == (numPhotos - 1)) {
           nextPhoto = photos[0];
         } else {
           nextPhoto = photos[shownPhotoIndex + 1];
         }
-        
         // Hide current photo
-           // Add the "hide" class to the list of existing classes
-           photos[shownPhotoIndex].className += " hide";
-        
+        // Add the "hide" class to the list of existing classes
+        photos[shownPhotoIndex].className += " hide";
         // Show next photo by removing "hide" class
-          // Create a new string with classes that removes "hide"
-          var newClass= nextPhoto.className.replace("hide", "");
-          nextPhoto.className = newClass;
-      } // end next click
-      
+        // Create a new string with classes that removes "hide"
+        var newClass = nextPhoto.className.replace("hide", "");
+        nextPhoto.className = newClass;
+    } // end next click
     var previous = document.getElementById("previous");
     previous.onclick = function() {
         var prevPhoto;
@@ -100,30 +84,16 @@ function progressSlides() {
         }
         // Hide current photo
         photos[shownPhotoIndex].className += " hide";
-        
-       // Show previous photo
-        var newClass= prevPhoto.className.replace("hide", "");
+        // Show previous photo
+        var newClass = prevPhoto.className.replace("hide", "");
         prevPhoto.className = newClass;
-        
-        
-        
-      } // end previous click
+    } // end previous click
   } // end progressSlides
-
-// Run JS after images have downloaded
-window.onload = function(){
-    addControls();
-    progressSlides();
+  // Run JS after images have downloaded
+  
+window.onload = function() {
+  addControls();
+  progressSlides();
 }
 
-
 hidePhotos();
-
-
-
-
-
-
-
-
-
